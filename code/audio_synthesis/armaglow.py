619a90497468f25233abf6b9ee6f20159566c89e
Ryan Downing <ryansdowning1@gmail.com>
2021-12-17 16:49:43 -0500

    audio synthesis code including armaglow and evaluation metrics

---
+++ code/audio_synthesis/armaglow.py
@@ -0,0 +1,345 @@
+import math
+import torch
+from torch.autograd import Variable
+import torch.nn.functional as F
+
+
+class ARMA1d(torch.nn.Module):
+    def __init__(
+            self,
+            channels,
+            kernel_size=1,
+            stride=1,
+            padding=0,
+            dilation=1,
+            groups=1,
+            bias=True,
+            a_kernel_size=3,
+            a_padding_mode="circular",
+            a_padding=0,
+            a_stride=1,
+            a_dilation=1,
+    ):
+        """
+        Initialization of a 1D-ARMA layer.
+        """
+        super(ARMA1d, self).__init__()
+
+        self.moving_average = torch.nn.Conv1d(
+            channels,
+            channels,
+            kernel_size,
+            stride=stride,
+            padding=padding,
+            dilation=dilation,
+            groups=groups,
+            bias=bias,
+        )
+
+        self.autoregressive = AutoRegressive1d(
+            channels,
+            a_kernel_size,
+            padding=a_padding,
+            padding_mode=a_padding_mode,
+            stride=a_stride,
+            dilation=a_dilation,
+        )
+
+    def forward(self, x):
+        """
+        Compuation of the 1D-ARMA layer.
+        """
+        # shape
+        batch_size, group_size, n_of_groups = x.size()
+
+        W = self.moving_average.weight.squeeze()
+
+        # Forward computation
+        log_det_W = batch_size * n_of_groups * torch.logdet(W.unsqueeze(0).float()).squeeze()
+        # size:[M, S, I1, I2]->[M, T, I1, I2]->[M, T, I1, I2]
+        x = self.moving_average(x)
+        x = self.autoregressive(x)
+        return x, log_det_W
+
+
+class AutoRegressive1d(torch.nn.Module):
+    def __init__(
+            self,
+            channels,
+            kernel_size=3,
+            padding=0,
+            padding_mode="circular",
+            stride=1,
+            dilation=1,
+    ):
+        """
+        Initialization of 1D-AutoRegressive layer.
+        """
+        super(AutoRegressive1d, self).__init__()
+
+        if padding_mode == "circular":
+            self.a = AutoRegressiveCircular(channels, kernel_size, padding, stride, dilation)
+        elif paddind_mode == "reflect":
+            raise NotImplementedError
+        else:
+            raise NotImplementedError
+
+    def forward(self, x):
+        """
+        Computation of 1D-AutoRegressive layer.
+        """
+        x = self.a(x)
+        return x
+
+
+class AutoRegressiveCircular(torch.nn.Module):
+    def __init__(self, channels, kernel_size, padding=3, stride=1, dilation=1):
+        """
+        Initialization of a 1D-AutoRegressive layer (with circular padding).
+        """
+        super(AutoRegressiveCircular, self).__init__()
+        self.alpha = torch.nn.Conv1d(
+            channels,
+            channels,
+            kernel_size // 2,
+            padding=padding,
+            padding_mode='circular',
+            stride=stride,
+            dilation=dilation
+        )
+        #torch.nn.Parameter(torch.Tensor(channels, kernel_size // 2, 4))  # size:[T, P, 4]
+
+    def forward(self, x):
+        """
+        Computation of the 1D-AutoRegressive layer (with circular padding).
+        """
+        return self.alpha(x)
+
+@torch.jit.script
+def fused_add_tanh_sigmoid_multiply(input_a, input_b, n_channels):
+    n_channels_int = n_channels[0]
+    in_act = input_a + input_b
+    t_act = torch.tanh(in_act[:, :n_channels_int, :])
+    s_act = torch.sigmoid(in_act[:, n_channels_int:, :])
+    acts = t_act * s_act
+    return acts
+
+
+class Invertible1x1Conv(torch.nn.Module):
+    """
+    The layer outputs both the convolution, and the log determinant
+    of its weight matrix.  If reverse=True it does convolution with
+    inverse
+    """
+
+    def __init__(self, c):
+        super(Invertible1x1Conv, self).__init__()
+        self.conv = torch.nn.Conv1d(c, c, kernel_size=1, stride=1, padding=0, bias=False)
+
+        # Sample a random orthonormal matrix to initialize weights
+        W = torch.qr(torch.FloatTensor(c, c).normal_())[0]
+
+        # Ensure determinant is 1.0 not -1.0
+        if torch.det(W) < 0:
+            W[:, 0] = -1 * W[:, 0]
+        W = W.view(c, c, 1)
+        W = W.contiguous()
+        self.conv.weight.data = W
+
+    def forward(self, z):
+        # shape
+        batch_size, group_size, n_of_groups = z.size()
+
+        W = self.conv.weight.squeeze()
+
+        # Forward computation
+        log_det_W = batch_size * n_of_groups * torch.logdet(W.unsqueeze(0).float()).squeeze()
+        z = self.conv(z)
+        return z, log_det_W
+
+    def infer(self, z):
+        # shape
+        batch_size, group_size, n_of_groups = z.size()
+
+        W = self.conv.weight.squeeze()
+
+        if not hasattr(self, "W_inverse"):
+            # Reverse computation
+            W_inverse = W.float().inverse()
+            W_inverse = Variable(W_inverse[..., None])
+            if z.type() == "torch.cuda.HalfTensor" or z.type() == "torch.HalfTensor":
+                W_inverse = W_inverse.half()
+            self.W_inverse = W_inverse
+        z = F.conv1d(z, self.W_inverse, bias=None, stride=1, padding=0)
+        return z
+
+
+class WN(torch.nn.Module):
+    """
+    This is the WaveNet like layer for the affine coupling.  The primary
+    difference from WaveNet is the convolutions need not be causal.  There is
+    also no dilation size reset.  The dilation only doubles on each layer
+    """
+
+    def __init__(
+        self,
+        n_in_channels,
+        n_mel_channels,
+        n_layers,
+        n_channels,
+        kernel_size,
+    ):
+        super(WN, self).__init__()
+        assert kernel_size % 2 == 1
+        assert n_channels % 2 == 0
+        self.n_layers = n_layers
+        self.n_channels = n_channels
+        self.in_layers = torch.nn.ModuleList()
+        self.res_skip_layers = torch.nn.ModuleList()
+        self.cond_layers = torch.nn.ModuleList()
+
+        start = torch.nn.Conv1d(n_in_channels, n_channels, 1)
+        start = torch.nn.utils.weight_norm(start, name="weight")
+        self.start = start
+
+        # Initializing last layer to 0 makes the affine coupling layers
+        # do nothing at first.  This helps with training stability
+        end = torch.nn.Conv1d(n_channels, 2 * n_in_channels, 1)
+        end.weight.data.zero_()
+        end.bias.data.zero_()
+        self.end = end
+
+        for i in range(n_layers):
+            dilation = 2 ** i
+            padding = int((kernel_size * dilation - dilation) / 2)
+            in_layer = torch.nn.Conv1d(
+                    n_channels,
+                    2 * n_channels,
+                    kernel_size,
+                    dilation=dilation,
+                    padding=padding,
+            )
+            in_layer = torch.nn.utils.weight_norm(in_layer, name="weight")
+            self.in_layers.append(in_layer)
+
+            cond_layer = torch.nn.Conv1d(n_mel_channels, 2 * n_channels, 1)
+            cond_layer = torch.nn.utils.weight_norm(cond_layer, name="weight")
+            self.cond_layers.append(cond_layer)
+
+            # last one is not necessary
+            if i < n_layers - 1:
+                res_skip_channels = 2 * n_channels
+            else:
+                res_skip_channels = n_channels
+            res_skip_layer = torch.nn.Conv1d(n_channels, res_skip_channels, 1)
+            res_skip_layer = torch.nn.utils.weight_norm(res_skip_layer, name="weight")
+            self.res_skip_layers.append(res_skip_layer)
+
+    def forward(self, forward_input):
+        audio, spect = forward_input
+        audio = self.start(audio)
+
+        for i in range(self.n_layers):
+            acts = fused_add_tanh_sigmoid_multiply(
+                self.in_layers[i](audio),
+                self.cond_layers[i](spect),
+                torch.IntTensor([self.n_channels]),
+            )
+
+            res_skip_acts = self.res_skip_layers[i](acts)
+            if i < self.n_layers - 1:
+                audio = res_skip_acts[:, : self.n_channels, :] + audio
+                skip_acts = res_skip_acts[:, self.n_channels :, :]
+            else:
+                skip_acts = res_skip_acts
+
+            if i == 0:
+                output = skip_acts
+            else:
+                output = skip_acts + output
+        return self.end(output)
+
+
+class WaveGlow(torch.nn.Module):
+    def __init__(
+        self,
+        n_mel_channels,
+        n_flows,
+        n_group,
+        n_early_every,
+        n_early_size,
+        WN_config,
+        arma=False,
+    ):
+        super(WaveGlow, self).__init__()
+
+        self.upsample = torch.nn.ConvTranspose1d(n_mel_channels, n_mel_channels, 1024, stride=256)
+        assert n_group % 2 == 0
+        self.n_flows = n_flows
+        self.n_group = n_group
+        self.n_early_every = n_early_every
+        self.n_early_size = n_early_size
+        self.WN = torch.nn.ModuleList()
+        self.convinv = torch.nn.ModuleList()
+
+        n_half = int(n_group / 2)
+
+        # Set up layers with the right sizes based on how many dimensions
+        # have been output already
+        n_remaining_channels = n_group
+        if arma:
+            Conv = lambda channels: ARMA1d(channels)
+        else:
+            Conv = lambda channels: Invertible1x1Conv(channels)
+        for k in range(n_flows):
+            if k % self.n_early_every == 0 and k > 0:
+                n_half = n_half - int(self.n_early_size / 2)
+                n_remaining_channels = n_remaining_channels - self.n_early_size
+            self.convinv.append(Conv(n_remaining_channels))
+            self.WN.append(WN(n_half, n_mel_channels * n_group, **WN_config))
+        self.n_remaining_channels = n_remaining_channels
+
+    def forward(self, forward_input):
+        """
+        forward_input[0] = mel_spectrogram:  batch x n_mel_channels x frames
+        forward_input[1] = audio: batch x time
+        """
+        spect, audio = forward_input
+
+        #  Upsample spectrogram to size of audio
+        spect = self.upsample(spect)
+        assert spect.size(2) >= audio.size(1)
+        if spect.size(2) > audio.size(1):
+            spect = spect[:, :, : audio.size(1)]
+
+        spect = spect.unfold(2, self.n_group, self.n_group).permute(0, 2, 1, 3)
+        spect = spect.contiguous().view(spect.size(0), spect.size(1), -1)
+        spect = spect.permute(0, 2, 1)
+
+        audio = audio.unfold(1, self.n_group, self.n_group).permute(0, 2, 1)
+        output_audio = []
+        log_s_list = []
+        log_det_W_list = []
+
+        for k in range(self.n_flows):
+            if k % self.n_early_every == 0 and k > 0:
+                output_audio.append(audio[:, : self.n_early_size, :])
+                audio = audio[:, self.n_early_size :, :]
+
+            audio, log_det_W = self.convinv[k](audio)
+            log_det_W_list.append(log_det_W)
+
+            n_half = int(audio.size(1) // 2)
+            audio_0 = audio[:, :n_half, :]
+            audio_1 = audio[:, n_half:]
+
+            output = self.WN[k]((audio_0, spect))
+            log_s = output[:, n_half:, :]
+            b = output[:, :n_half, :]
+            audio_1 = torch.exp(log_s) * audio_1 + b
+            log_s_list.append(log_s)
+
+            audio = torch.cat([audio_0, audio_1], 1)
+
+        output_audio.append(audio)
+        return torch.cat(output_audio, 1), log_s_list, log_det_W_list
---
+++ code/audio_synthesis/eval_metrics.py
@@ -0,0 +1,164 @@
+import glob
+import warnings
+from pathlib import Path
+from typing import Any, Callable, Iterable, Optional, Type
+
+import numpy as np
+import torch
+from scipy.io import wavfile
+from torch.nn import functional as F
+from torch.utils.data import DataLoader
+from torch.utils.data.dataset import Dataset
+
+
+class WavDataSet(Dataset):
+    """Torch dataset for wavfile directories"""
+
+    def __init__(
+        self,
+        samples: str,
+        labels: Optional[Iterable[Any]] = None,
+        transform: Optional[Callable[[np.ndarray], Any]] = None,
+        train: bool = True,
+        dtype: Type = torch.FloatTensor,
+    ):
+        """
+        Args:
+            samples: Path to directory containing audio samples (wav files are supported)
+            transform: Optionally provide a preprocessing function to transform the audio data before predicting the
+                       class with the classifier
+            dtype: Datatype to cast the loaded numpy array of audio data to (done before passing to transform function)
+        """
+        self.files = glob.glob(os.path.join(Path(samples), "*.wav"))
+        self.labels = np.array(labels) if labels is not None else None
+        self.transform = transform if transform is not None else lambda audio: audio
+        self.train = train
+        self.dtype = dtype
+
+        if not train:
+            if labels is None:
+                raise ValueError("Cannot create test dataloader without labels")
+            if labels.shape[0] != len(self.files):
+                raise ValueError(
+                    f"The number of labels provided does not match the number of samples, got {labels.shape[0]} labels"
+                    f" and {len(self.files)} samples"
+                )
+
+    def __len__(self):
+        return len(self.files)
+
+    def __getitem__(self, item):
+        file = self.files[item]
+        _, audio = wavfile.read(file)
+        audio = self.transform(torch.from_numpy(audio).to(dtype=self.dtype))
+        return audio if self.train else audio, self.labels[item]
+
+
+def _check_cuda(cuda: bool):
+    # Make sure cuda is available if using cuda
+    if cuda and not torch.cuda.is_available():
+        raise EnvironmentError("CUDA set to true but no CUDA enabled device available")
+    # Warn if cuda is available and not using
+    if not cuda and torch.cuda.is_available():
+        warnings.warn("A CUDA enabled device is available, but cuda is not set to True")
+
+
+def audio_inception_score(
+    classifier: Callable[..., np.ndarray],
+    samples: Union[str, Path],
+    transform: Optional[Callable[[np.ndarray], Any]] = None,
+    batch_size: int = 4,
+    splits: int = 10,
+    n_classes: int = 10,
+    shuffle: bool = True,
+    cuda: bool = True,
+) -> np.ndarray:
+    """Inception score implementation adapted for audio synthesis performance evaluation
+
+    Based on https://github.com/openai/improved-gan/blob/master/inception_score/model.py
+    From Improved Techniques for Training GANs (Goodfellow, 2016) https://arxiv.org/pdf/1606.03498.pdf
+
+    Args:
+        classifier: Classification model (in evaluation mode) which classifies an audio sample into <n_classes> by
+                    computing confidence scores for each class, for each sample
+        samples: Path to directory containing audio samples (wav files are supported)
+        transform: Optionally provide a preprocessing function to transform the audio data before predicting the class
+                   with the classifier
+        batch_size: Integer representing the number of samples to predict on in each iteration
+        splits: Integer representing the number of splits to chunk the predictions into, producing an inception score
+                for each chunk
+        n_classes: The number of classes predicted by the classification model
+        shuffle: Boolean flag, whether or not to shuffle the dataset
+        cuda: Boolean flag, whether or not to use a CUDA device for the classification model
+
+    Returns:
+        <splits> x 1 np.ndarray containing the computed inception score for each split
+    """
+    _check_cuda(cuda)
+    dtype = torch.cuda.FloatTensor if cuda else torch.FloatTensor  # CUDA type if on cuda
+    dataloader = DataLoader(
+        WavDataSet(samples, None, transform, False, dtype), batch_size=batch_size, shuffle=shuffle, num_workers=0
+    )
+
+    # Must have >= 1 sample per split
+    n = len(dataloader.dataset)
+    if splits > n:
+        raise ValueError(f"Cannot compute inception score for {splits} splits from only {n} samples")
+
+    # Process classification predictions in batches
+    predictions = np.empty((n, n_classes), dtype=np.float64)
+    for i, batch in enumerate(dataloader):
+        preds = classifier(batch)
+        preds = F.softmax(preds).data.cpu().numpy()
+        predictions[i * batch_size : (i + 1) * batch_size] = preds
+
+    # Compute inception scores
+    scores = np.empty(splits, dtype=np.float64)
+    split_size = n // splits
+    for i in range(splits):
+        preds_split = predictions[i * split_size : (i + 1) * split_size]
+        kl = preds_split * (np.log(preds_split) - np.log(np.expand_dims(np.mean(preds_split, axis=0), axis=0)))
+        kl = np.exp(np.mean(np.sum(kl, axis=1)))
+        scores[i] = kl
+    return scores
+
+
+def pitch_accuracy_entropy(
+    classifier: Callable[..., np.ndarray],
+    samples: str,
+    labels: np.ndarray,
+    transform: Optional[Callable[[np.ndarray], Any]] = None,
+    batch_size: int = 4,
+    shuffle: bool = True,
+    cuda: bool = True,
+):
+    """Implementation of pitch accuracy and pitch entropy as described in GANSynth: Adversarial Neural Audio Synthesis
+    (Engel, 2019) https://arxiv.org/abs/1902.08710
+
+    Args:
+        classifier: Classification model (in evaluation mode) which classifies an audio sample into <n_classes> by
+                    computing confidence scores for each class, for each sample
+        samples: Path to directory containing audio samples (wav files are supported)
+        labels: Numpy array of integers representing the true label for each corresponding sample (index of label)
+        transform: Optionally provide a preprocessing function to transform the audio data before predicting the class
+                   with the classifier
+        batch_size: Integer representing the number of samples to predict on in each iteration
+        shuffle: Boolean flag, whether or not to shuffle the dataset
+        cuda: Boolean flag, whether or not to use a CUDA device for the classification model
+
+    Returns:
+        <splits> x 1 np.ndarray containing the computed inception score for each split
+    """
+    _check_cuda(cuda)
+    dtype = torch.cuda.FloatTensor if cuda else torch.FloatTensor  # CUDA type if on cuda
+    dataloader = DataLoader(
+        WavDataSet(samples, labels, transform, True, dtype), batch_size=batch_size, shuffle=shuffle, num_workers=0
+    )
+
+    predictions = np.empty(len(dataloader.dataset), dtype=np.int32)
+    for i, batch in enumerate(dataloader):
+        preds = classifier(batch)
+        preds = F.argmax(preds).data.cpu().numpy()
+        predictions[i * batch_size : (i + 1) * batch_size] = preds
+    probs = np.array([(predictions == i).mean() for i in range(labels.min(), labels.max() + 1)], dtype=np.float64)
+    return (labels == predictions).mean(), -(probs @ np.log(probs))  # Compute accuracy and entropy of predictions
